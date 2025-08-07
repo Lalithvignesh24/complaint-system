5990dc2eaa8afb602add7e5ad050616b9488f7d0
Lalithvignesh24 <andhelalithvignesh@gmail.com>
2025-08-07 15:27:45 +0530

    Resolved merge conflict and updated code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,12 +6,9 @@ const generateToken = (id) => {
         expiresIn: '1d', // Token expires in 1 day
     });
 };
-<<<<<<< HEAD
 <div>
-    <p>hi</p>
+    <p>hi ra ungaaa </p>
 </div>
-=======
->>>>>>> 12b1b9f21a52117b3e85ba00487a10805b53e613
 
 exports.loginAdmin = async (req, res) => {
     const { email, password } = req.body;
@@ -32,8 +29,4 @@ exports.loginAdmin = async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 };//hi
-<<<<<<< HEAD
-//hi mowaaa
-=======
-// backend/controllers/authController.js
->>>>>>> 12b1b9f21a52117b3e85ba00487a10805b53e613
+
